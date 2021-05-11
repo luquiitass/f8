b4e3a03d98fbb7e127994743f855abce5d15ae62
lucas <liquiitass@gmail.com>
2021-05-11 01:22:07 -0300

    dep

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //server_url : 'http://192.168.1.15/f8a/public/api'
-  server_url : 'http://34.68.147.207/f8a/public/api'
+  server_url : 'https://34.68.147.207/f8a/public/api'
 
 };
